7d76c18bf2e65b32b4d9b09a8f1f4ae3bc0f2429
Александров Федор Алексеевич <fedor.aleksandrov@etalongroup.com>
2023-07-18 00:48:06 +0300

    6.2 Шаг 2 · Расширение сервиса для работы с пользователями

--- TaskManagerCourse.Client/Services/UsersRequestService.cs
+++ TaskManagerCourse.Client/Services/UsersRequestService.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TaskManagerCourse.Client.Models;
+using TaskManagerCourse.Common.Models;
 
 namespace TaskManagerCourse.Client.Services
 {
@@ -14,25 +16,57 @@ namespace TaskManagerCourse.Client.Services
     {
         private const string HOST = "http://localhost:55674/api/";
 
-        private string GetDataByUrl(string url, string userName, string password)
+        private string _userController = HOST + "users";
+
+        private string GetDataByUrl(string url, string userName = null, string password = null)
         {
             string result = string.Empty;
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
             request.Method = "POST";
+
             if (userName != null && password != null)
             {
                 string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
                 request.Headers.Add("Authorization", "Basic " + encoded);
-
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                {
-                    string responseStr = reader.ReadToEnd();
-                    result = responseStr;
-                }
             };
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                string responseStr = reader.ReadToEnd();
+                result = responseStr;
+            }
             return result;
         }
+
+        private HttpStatusCode SendDataByUrl(HttpMethod method, string url, AuthToken token, string data)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            if (method == HttpMethod.Post)
+                result = client.PostAsync(url, content).Result;
+
+            if (method == HttpMethod.Patch)
+                result = client.PatchAsync(url, content).Result;
+
+            return result.StatusCode;
+        }
+
+        private HttpStatusCode DeleteDataByUrl(string url, AuthToken token)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+
+            result = client.DeleteAsync(url).Result;
+
+            return result.StatusCode;
+        }
+
+
         public AuthToken GetToken(string userName, string password)
         {
             string url = HOST + "account/token";
@@ -40,5 +74,39 @@ namespace TaskManagerCourse.Client.Services
             AuthToken token = JsonConvert.DeserializeObject<AuthToken>(resultStr);
             return token;
         }
+
+        public HttpStatusCode CreateUser(AuthToken token, UserModel user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Post, _userController, token, userJson);
+            return result;
+        }
+
+        public List<UserModel> GetAllUsers(AuthToken token)
+        {
+            string response = GetDataByUrl(_userController);
+            List<UserModel> users = JsonConvert.DeserializeObject<List<UserModel>>(response);
+            return users;
+        }
+        public HttpStatusCode DeleteUser(AuthToken token, int userId)
+        {
+            var result = DeleteDataByUrl(_userController + $"/{userId}" , token);
+            return result;
+        }
+
+        public HttpStatusCode CreateMultipleUser(AuthToken token, List<UserModel> users)
+        {
+            string userJson = JsonConvert.SerializeObject(users);
+            var result = SendDataByUrl(HttpMethod.Post, _userController+ "/all", token, userJson);
+            return result;
+        }
+
+        public HttpStatusCode UpdateUser(AuthToken token, UserModel user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Patch, _userController + $"/{user.Id}", token, userJson);
+            return result;
+        }
+
     }
 }
