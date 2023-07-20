259a34b535d277a13c9d275bd99bb3cd30b44692
Александров Федор Алексеевич <fedor.aleksandrov@etalongroup.com>
2023-07-20 23:38:25 +0300

    6.6 Шаг 2 · DesksRequestService. Сервис для работы с досками

--- TaskManagerCourse.Client/Services/CommonRequestService.cs
+++ TaskManagerCourse.Client/Services/CommonRequestService.cs
@@ -14,27 +14,62 @@ namespace TaskManagerCourse.Client.Services
     {
         public const string HOST = "http://localhost:55674/api/";
 
-        protected string GetDataByUrl(HttpMethod method, string url, AuthToken token, string userName = null, string password = null)
+        protected string GetDataByUrl(HttpMethod method, string url, AuthToken token, string userName = null, string password = null,
+            Dictionary<string,string> parameters = null)
         {
-            string result = string.Empty;
-            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.Method = method.Method;
+            //string result = string.Empty;
+            //HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
+            //request.Method = method.Method;
 
-            if (userName != null && password != null)
+            //if (userName != null && password != null)
+            //{
+            //    string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
+            //    request.Headers.Add("Authorization", "Basic " + encoded);
+            //}
+            //else if (token != null)
+            //{
+            //    request.Headers.Add("Authorization", "Bearer " + token.access_token);
+            //}
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            //{
+            //    string responseStr = reader.ReadToEnd();
+            //    result = responseStr;
+            //}
+            //return result;
+
+            //******************
+            WebClient client = new WebClient();
+            if(userName != null && password != null)
             {
                 string encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
-                request.Headers.Add("Authorization", "Basic " + encoded);
+                client.Headers.Add("Authorization", "Basic " + encoded);
             }
             else if (token != null)
             {
-                request.Headers.Add("Authorization", "Bearer " + token.access_token);
+                client.Headers.Add("Authorization", "Bearer " + token.access_token);
             }
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+
+            if (parameters != null)
             {
-                string responseStr = reader.ReadToEnd();
-                result = responseStr;
+                foreach (var key in parameters.Keys)
+                {
+                    client.QueryString.Add(key, parameters[key]);
+                };
             }
+            client.QueryString.Add("", "");
+
+            byte[] data = Array.Empty<byte>();
+            if (method == HttpMethod.Post)
+            {
+                data = client.UploadValues(url, method.Method, client.QueryString);
+            }
+            if (method == HttpMethod.Get)
+            {
+                data = client.DownloadData(url);
+            }
+            //var data = client.UploadValues(url, method.Method, client.QueryString);
+            string result = UnicodeEncoding.UTF8.GetString(data);
             return result;
         }
 
---
+++ TaskManagerCourse.Client/Services/DesksRequestService.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using TaskManagerCourse.Client.Models;
+using TaskManagerCourse.Common.Models;
+
+namespace TaskManagerCourse.Client.Services
+{
+    public class DesksRequestService : CommonRequestService
+    {
+        private string _desksControllerUrl = HOST + "desks";
+
+        public List<DeskModel> GetAllDesks(AuthToken token)
+        {
+            string response = GetDataByUrl(HttpMethod.Get, _desksControllerUrl, token);
+            List<DeskModel> desks = JsonConvert.DeserializeObject<List<DeskModel>>(response);
+            return desks;
+        }
+
+        public DeskModel GetDesksById(AuthToken token, int deskId)
+        {
+            var response = GetDataByUrl(HttpMethod.Get, _desksControllerUrl + $"/{deskId}", token);
+            DeskModel desk = JsonConvert.DeserializeObject<DeskModel>(response);
+            return desk;
+        }
+        public List<DeskModel> GetDeskByProject(AuthToken token, int projectId)
+        {
+            var parameters = new Dictionary<string, string>();
+            parameters.Add("projectId",projectId.ToString());
+            string response = GetDataByUrl(HttpMethod.Get, _desksControllerUrl+ "/project", token, null,null,parameters);
+            List<DeskModel> desks = JsonConvert.DeserializeObject<List<DeskModel>>(response);
+            return desks;
+        }
+
+        public HttpStatusCode CreateDesk(AuthToken token, DeskModel desk)
+        {
+            string deskJson = JsonConvert.SerializeObject(desk);
+            var result = SendDataByUrl(HttpMethod.Post, _desksControllerUrl, token, deskJson);
+            return result;
+        }
+
+        public HttpStatusCode UpdateDesk(AuthToken token, DeskModel desk)
+        {
+            string deskJson = JsonConvert.SerializeObject(desk);
+            var result = SendDataByUrl(HttpMethod.Patch, _desksControllerUrl + $"/{desk.Id}", token, deskJson);
+            return result;
+        }
+
+
+        public HttpStatusCode DeleteDesk(AuthToken token, int deskId)
+        {
+            var result = DeleteDataByUrl(_desksControllerUrl + $"/{deskId}", token);
+            return result;
+        }
+    }
+}
